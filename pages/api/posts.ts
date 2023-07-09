d107817f309feb562a5163eacff73e5571870521
Kim <kim.mauritius@gmail.com>
2023-07-09 15:00:14 +0200

    created an API fetching data from folders

---
+++ pages/api/posts.ts
@@ -0,0 +1,30 @@
+import matter from "gray-matter";
+import { NextApiHandler } from "next";
+const fs = require('fs');
+const path = require('path');
+
+const handler: NextApiHandler = (req, res) => {
+    const { method } = req;
+
+    switch (method) {
+        case 'GET': {
+            const data = readPostsInfo();
+            return res.status(200).json({ postInfo: data});
+        }
+        default:
+            return res.status(404).send('Not found');
+    }
+};
+
+const readPostsInfo = () => {
+    const dirPathToRead = path.join(process.cwd(), 'posts');
+    const dirs = fs.readdirSync(dirPathToRead);
+    const data = dirs.map((filename: string) => {
+        const filePath = path.join(process.cwd(), "posts/" + filename);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        return matter(fileContents).data;
+    });
+    return data;
+}
+
+export default handler;
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,9 +1,26 @@
-import { NextPage } from 'next';
+import BlogCard from '@/component/BlogCard';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
 
-interface Props {}
 
-const blogs: NextPage<Props> = () => {
- return <div>blogs</div>;
+export const getStaticProps: GetStaticProps = async() => {
+    const res = await fetch('http://localhost:3000/api/posts').then((data) => data.json());
+    return {
+        props: {
+            posts: res.postInfo,
+        },
+    };
+
+};
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const blogs: NextPage<Props> = ({posts}) => {
+    
+ return <div className='max-w-3xl mx-auto p-5 space-y-5'>
+    {posts.map((post: { title: string; meta: string; }) => (
+        <BlogCard title={post.title} desc={post.meta}/>
+    ))}
+
+ </div>;
 };
 
 export default blogs;
